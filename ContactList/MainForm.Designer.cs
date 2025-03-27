acbe62445cff364ace3fe713b672fb9d59ccd4a3
Seven <chametot5609@gmail.com>
2025-03-27 15:26:58 +0700

    Xóa Form thêm liên hệ

--- ContactList/MainForm.Designer.cs
+++ ContactList/MainForm.Designer.cs
@@ -45,7 +45,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(97, 159);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 32);
             this.button1.TabIndex = 1;
@@ -59,9 +59,10 @@
             this.ClientSize = new System.Drawing.Size(624, 387);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "Form1";
+
             this.ResumeLayout(false);
             this.PerformLayout();
 
